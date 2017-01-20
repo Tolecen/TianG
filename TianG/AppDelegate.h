b073986174e836327728999a57c4532664bc530d
Tolecen <taox.l@163.com>
2017-01-20 09:20:26 +0800

    Initial Commit

---
+++ TianG/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TianG
+//
+//  Created by Xinle on 2017/1/20.
+//  Copyright © 2017年 TaoXinle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TianG/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TianG
+//
+//  Created by Xinle on 2017/1/20.
+//  Copyright © 2017年 TaoXinle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
