b56cfbcbdcf2ca4a1ab2be6dfba03f79803dbe2a
Tolecen <taox.l@163.com>
2017-01-22 15:58:53 +0800

    xs

--- TianG/ViewController.m
+++ TianG/ViewController.m
@@ -7,9 +7,12 @@
 //
 
 #import "ViewController.h"
+#import "AFNetworking.h"
 NSString * const url_string =  @"http://139.196.21.231:8886/";
-@interface ViewController ()<UIWebViewDelegate>
-
+@interface ViewController ()<UIWebViewDelegate,UIAlertViewDelegate>
+{
+    UIWebView * webViewP;
+}
 @end
 
 @implementation ViewController
@@ -22,15 +25,15 @@ NSString * const url_string =  @"http://139.196.21.231:8886/";
     
     bgt.backgroundColor = [UIColor colorWithRed:38.0f/255.0f green:37.0f/255.0f blue:44.0f/255.0f alpha:1];
     
-    UIWebView *webView = [[UIWebView alloc] initWithFrame:CGRectMake(0, 20, CGRectGetWidth(self.view.frame),
+    webViewP = [[UIWebView alloc] initWithFrame:CGRectMake(0, 20, CGRectGetWidth(self.view.frame),
                                                                      CGRectGetHeight(self.view.frame)-20)];
-    webView.allowsInlineMediaPlayback = YES;
-    webView.scalesPageToFit = YES;
-    webView.scrollView.bounces = NO;
-    webView.delegate = self;
-    [webView setOpaque:NO];
-    [webView setBackgroundColor:[UIColor colorWithRed:0.9 green:0.9 blue:0.9 alpha:1]];
-    [self.view addSubview:webView];
+    webViewP.allowsInlineMediaPlayback = YES;
+    webViewP.scalesPageToFit = YES;
+    webViewP.scrollView.bounces = NO;
+    webViewP.delegate = self;
+    [webViewP setOpaque:NO];
+    [webViewP setBackgroundColor:[UIColor colorWithRed:0.9 green:0.9 blue:0.9 alpha:1]];
+    [self.view addSubview:webViewP];
     
     
 //    NSString * lastUrlStr = [[NSUserDefaults standardUserDefaults] objectForKey:@"lastUrl"];
@@ -38,9 +41,63 @@ NSString * const url_string =  @"http://139.196.21.231:8886/";
 //        [webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:lastUrlStr]]];
 //    }
 //    else
-       [webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:url_string]]];
+//       [webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:url_string]]];
+    
+    [self requestWithParameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        
+    }];
 
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+-(void)requestWithParameters:(NSDictionary *)parameters  success:(void (^)(AFHTTPRequestOperation *operation, id responseObject))success failure:(void (^)(AFHTTPRequestOperation *operation, NSError *error))failure
+    {
+        __weak __typeof(&*self)weakSelf = self;
+        [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:YES];
+        
+        NSString * reqUrlStr = [url_string stringByAppendingString:@"Customer/FlndPwd"];
+    
+        AFHTTPClient *httpClient = [[AFHTTPClient alloc] initWithBaseURL:[NSURL URLWithString:reqUrlStr]];
+        [httpClient setParameterEncoding:AFJSONParameterEncoding];
+        [httpClient getPath:@"" parameters:parameters success:^(AFHTTPRequestOperation *operation, id responseObject) {
+            //       NSData *filData = [responseObject newAESDecryptWithPassphrase:CHONGWUSHUONETPASSWORD];
+            if (responseObject) {
+                NSString *resText = [[NSString alloc] initWithData:responseObject encoding:NSUTF8StringEncoding];
+                if (resText && [resText isEqualToString:@"view"]) {
+                    [webViewP loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:url_string]]];
+                }
+                else
+                {
+                    [weakSelf showError];
+                }
+            }
+            else
+                [weakSelf showError];
+            
+           
+            [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
+        } failure:^(AFHTTPRequestOperation *operation, NSError *error){
+            
+            [weakSelf showError];
+            [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
+        }];
+        
+        
+    
+
+}
+
+-(void)showError
+{
+    UIAlertView * alert = [[UIAlertView alloc] initWithTitle:@"提示" message:@"软件暂时不可用" delegate:self cancelButtonTitle:@"知道了" otherButtonTitles:nil, nil];
+    [alert show];
+}
+
+-(void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    exit(0);
 }
 
 #pragma mark - UIWebViewDelegate
